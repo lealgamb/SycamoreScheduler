b4f6fadb4df76964503c776c569fa5129eb139f3
James Meyer <jamesmey@usc.edu>
2018-11-11 13:23:36 -0800

    Update insert_instructors.py

--- database/insert_instructors.py
+++ database/insert_instructors.py
@@ -5,13 +5,12 @@ if __name__ == "__main__":
     instructorID = 0
     instructorName = ""
     rating = 0.0
-    print(rating_dict)
     for key in rating_dict:
-        instructorName = key
+        instructorName = str(key).replace("\"", "\\\"")
         instructorID = int(rating_dict[key]['id'])
         try:
             rating = float(rating_dict[key]['rating'])
         except:
             pass
-        print("INSERT INTO Instructor(instructorID, instructorName, rmpRating) VALUES (" + str(instructorID) + "\", "
+        print("INSERT INTO Instructor(instructorID, instructorName, rmpRating) VALUES (" + str(instructorID) + ", \""
                 + instructorName + "\", " + str(rating) + ");")
