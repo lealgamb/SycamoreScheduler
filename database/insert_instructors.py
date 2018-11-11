0eeffa37447298ebe94888716ef6f2117805741d
James Meyer <jamesmey@usc.edu>
2018-11-11 13:13:21 -0800

    Create files to insert instructors to db

---
+++ database/insert_instructors.py
@@ -0,0 +1,17 @@
+import json
+
+if __name__ == "__main__":
+    rating_dict = json.load(open('ratings.json'))
+    instructorID = 0
+    instructorName = ""
+    rating = 0.0
+    print(rating_dict)
+    for key in rating_dict:
+        instructorName = key
+        instructorID = int(rating_dict[key]['id'])
+        try:
+            rating = float(rating_dict[key]['rating'])
+        except:
+            pass
+        print("INSERT INTO Instructor(instructorID, instructorName, rmpRating) VALUES (" + str(instructorID) + ", "
+                + instructorName + ", " + str(rating) + ");")
