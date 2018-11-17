f3b47e6122ad99d656c0705fe39d5686679f7961
Sajeev Saluja <sajeevsaluja@gmail.com>
2018-11-16 16:21:34 -0800

    updating WebContent and sycamore-react

--- sycamore-eclipse/WebContent/precache-manifest.db71e275254af1aa35e452321bbd3f05.js
+++ sycamore-eclipse/WebContent/precache-manifest.db71e275254af1aa35e452321bbd3f05.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/sycamore-scheduler/static/js/runtime~main.cea9536b.js"
   },
   {
-    "revision": "f268b57ab6c09d140d92",
-    "url": "/sycamore-scheduler/static/js/main.f268b57a.chunk.js"
+    "revision": "f326d0e0e5bfc0157ebb",
+    "url": "/sycamore-scheduler/static/js/main.f326d0e0.chunk.js"
   },
   {
     "revision": "5c01f817775a7090e501",
     "url": "/sycamore-scheduler/static/js/1.5c01f817.chunk.js"
   },
   {
-    "revision": "cacf9207b6c3ad6a1ee6af327ac65ef7",
+    "revision": "4aa3ff5d7a7ca6b1fbfb775095dc8d70",
     "url": "/sycamore-scheduler/index.html"
   }
 ];
