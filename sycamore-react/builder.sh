394fe03209c3841aca1b5685876d3bd00c64bed8
Sajeev Saluja <sajeevsaluja@gmail.com>
2018-11-18 03:40:33 -0800

    fixing dependencies

--- sycamore-react/builder.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-npm run build
-cd ../sycamore-eclipse/WebContent/
-rm -r *.js *.json index.html static favicon.ico
-cd ../../sycamore-react/build/
-cp -r * ../../sycamore-eclipse/WebContent/
-cd ../../sycamore-react
